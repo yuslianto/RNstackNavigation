7688dc9297f94ef03f02406c2b8d091ffbce9dc3
yuslianto <yuslianto56@gmail.com>
2019-07-08 08:57:01 +0700

    add buttom tab nacigator in App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   createStackNavigator,
   createAppContainer,
   createDrawerNavigator,
+  createBottomTabNavigator,
 } from "react-navigation";
 
 import HomeScreen from "./src/home";
@@ -10,6 +11,26 @@ import UserScreen from "./src/user";
 import LogoTitle from "./src/logo";
 import SideMenu from "./src/SideDrawer";
 
+const AppNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Users: UserScreen,
+},{
+  initialRouteName: 'Home',
+  tabBarOptions: {
+    activeTintColor: 'green',
+    inactiveTintColor: 'grey',
+    activeBackgroundColor: 'yellow',
+    inactiveBackgroundColor: 'blue',
+    style: {
+
+    },
+    labelStyle: {
+      fontSize: 20,
+    }
+  }
+});
+
+/**
 const AppNavigator = createDrawerNavigator({
   Home : {
     screen : HomeScreen,
@@ -25,7 +46,7 @@ const AppNavigator = createDrawerNavigator({
   drawerWidth: 200,
 
 });
-
+ */
 
 //const AppNavigator = createStackNavigator({
 //  Home : {
